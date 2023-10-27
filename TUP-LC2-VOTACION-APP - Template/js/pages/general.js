104cf58ae843469a79d595c54e2a850360cd1aea
Anabella <anabellarustici@gmail.com>
2023-10-27 17:33:00 -0300

    js generales select años

--- TUP-LC2-VOTACION-APP - Template/js/pages/general.js
+++ TUP-LC2-VOTACION-APP - Template/js/pages/general.js
@@ -1,31 +1,38 @@
 const tipoEleccion = 2;
 const tipoRecuento = 1;
 
-let anio = document.getElementById('select-anio');
+let selectAnio = document.getElementById('select-anio');
 
+document.addEventListener('DOMContentLoaded', function () {
+    //Se llama a la función cuando se carga la página
+    consultarCombo();
+});
 
 async function consultarCombo() {
-    if (anio.value == 0) {
-        alert('Seleccione un año');
-        return false;
-    }
-    else {
-        try {
-            const respuesta = await fetch("https://resultados.mininterior.gob.ar/api/menu/periodos");
 
-            if (respuesta.ok) {
-                //FUNCIONÓ
+    try {
+        const respuesta = await fetch("https://resultados.mininterior.gob.ar/api/menu/periodos");
+        if (respuesta.ok) {
+            //FUNCIONÓ
+            const data = await respuesta.json();
+            console.log(data);
 
-                const data = await respuesta.json();
+            //Se recorre la respuesta del json para llenar el combo de años con los años
+            data.forEach(anio => {
+                const option = document.createElement('option');
+                option.value = anio; // Se establece el valor del combo
+                option.textContent = anio.toString(); // Convertir a cadena para que el texto se pueda ver en el combo
+                selectAnio.appendChild(option); // Se agregan las opciones en el select
+            });
 
-                console.log(data);
-
-            } else {
-                console.log('Error 404');
-            }
-        }
-        catch (err) {
-            console.log(err);
+        } else {
+            console.log('Error 404');
         }
     }
+    catch (err) {
+        console.log(err);
+    }
+
 }
+
+
